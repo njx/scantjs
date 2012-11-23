cee012792761e4bc8db32aea1e017d812f6e846e
Narciso Jaramillo <njaramil@adobe.com>
2012-11-23 09:20:16 -0800

    Moved action functions into a separate 'do' object

--- scant.js
+++ scant.js
@@ -1,101 +1,145 @@
 (function ($) {
   var Scant = function ($root, defaults) {
-    this.defaults = $.extend({
+    this.do.defaults = $.extend({
       spacing: 20
     }, defaults);
-    this.$root = $root.css("position", "relative");
-    this.currentActions = this.actions = [];
+    
+    // actions set up by chained calls
+    this.actions = [];
+        
+    this.do.init($root);
   };
   
   $.extend(Scant.prototype, {
-    doMake: function (descriptor) {
-      var newNode = $("<div style='position: absolute'/>"), match;
-      
-      while (match = descriptor.match(/^[#\.][A-Za-z0-9-_]+/)) {
-        var value = match[0].slice(1);
-        if (match[0].charAt(0) === "#") {
-          newNode.attr("id", value);
-        } else {
-          newNode.addClass(value);
-        }
-        descriptor = descriptor.slice(match[0].length);
-      }
-      newNode.text(descriptor.trimLeft());
-      this.$root.append(newNode);
-      this.$current = newNode;
-    },
+    // The execution context that performs the actions set up by the initial chained calls
+    // to Scant. The actual chained functions are set up automatically based on the action
+    // functions available on the "do" object.
+    do: {
+      init: function ($root) {
+        // root node of our display
+        this.$root = $root.css("position", "relative");
+        
+        // node being operated on
+        this.$current = null;
+        
+        // outstanding promises
+        this.promises = [];
+        
+        // index of current action
+        this.currentAction = 0;       
     
-    doAt: function (left, top) {
-      this.$current.css({
-        left: left,
-        top: top
-      });
-    },
-    
-    doSize: function (width, height) {
-      this.$current.css({
-        width: width,
-        height: height
-      });
-    },
-    
-    doConnect: function () {
-      // *** TODO: no-op for now
-    },
-    
-    doRightOf: function (target) {
-      var $target = $(target);
-      this.$current.css({
-        left: $target.position().left + $target.outerWidth() + this.defaults.spacing,
-        top: $target.position().top
-      });
-    },
+        // bind methods
+        this._go = this._go.bind(this);
+      },
 
-    doLeftOf: function (target) {
-      var $target = $(target);
-      this.$current.css({
-        left: $target.position().left - this.defaults.spacing - this.$current.outerWidth(),
-        top: $target.position().top
-      });
+      make: function (descriptor) {
+        var newNode = $("<div style='position: absolute'/>"), match;
+        
+        while (match = descriptor.match(/^[#\.][A-Za-z0-9-_]+/)) {
+          var value = match[0].slice(1);
+          if (match[0].charAt(0) === "#") {
+            newNode.attr("id", value);
+          } else {
+            newNode.addClass(value);
+          }
+          descriptor = descriptor.slice(match[0].length);
+        }
+        newNode.text(descriptor.trimLeft());
+        this.$root.append(newNode);
+        this.$current = newNode;
+      },
+      
+      at: function (left, top) {
+        this.$current.css({
+          left: left,
+          top: top
+        });
+      },
+      
+      size: function (width, height) {
+        this.$current.css({
+          width: width,
+          height: height
+        });
+      },
+      
+      connect: function () {
+        // *** TODO: no-op for now
+      },
+      
+      fadeIn: function () {
+        // *** TODO: no-op for now
+      },
+      
+      rightOf: function (target) {
+        var $target = $(target);
+        this.$current.css({
+          left: $target.position().left + $target.outerWidth() + this.defaults.spacing,
+          top: $target.position().top
+        });
+      },
+  
+      leftOf: function (target) {
+        var $target = $(target);
+        this.$current.css({
+          left: $target.position().left - this.defaults.spacing - this.$current.outerWidth(),
+          top: $target.position().top
+        });
+      },
+      
+      above: function (target) {
+        var $target = $(target);
+        this.$current.css({
+          left: $target.position().left,
+          top: $target.position().top - this.defaults.spacing - this.$current.outerHeight()
+        });
+      },
+      
+      below: function (target) {
+        var $target = $(target);
+        this.$current.css({
+          left: $target.position().left,
+          top: $target.position().top + $target.outerHeight() + this.defaults.spacing
+        });
+      },
+      
+      next: function () {
+        var result = $.Deferred();
+        $(document.body).click(function () {
+          result.resolve();
+        });
+        return result;
+      },
+      
+      _go: function () {
+        var promise, action;
+        while (this.currentAction < this.actions.length) {
+          action = this.actions[this.currentAction++];
+          promise = action.func.apply(this, action.args);
+          if (promise) {
+            promise.done(this._go);
+            break;
+          }
+        }
+      },
     },
-    
-    doAbove: function (target) {
-      var $target = $(target);
-      this.$current.css({
-        left: $target.position().left,
-        top: $target.position().top - this.defaults.spacing - this.$current.outerHeight()
-      });
-    },
-    
-    doBelow: function (target) {
-      var $target = $(target);
-      this.$current.css({
-        left: $target.position().left,
-        top: $target.position().top + $target.outerHeight() + this.defaults.spacing
-      });
-    },
-    
+        
     go: function () {
-      var that = this;
-      this.actions.forEach(function (action) {
-          action.func.apply(that, action.args);
-      });
+      this.do.actions = this.actions;
+      this.do._go();
       return this;
     }
   });
   
   // Set up chaining functions to add to action list.
-  Object.keys(Scant.prototype).forEach(function (key) {
-    if (key.substr(0, 2) === "do" && key.charAt(2).toUpperCase() === key.charAt(2)) {
-      var chainFunc = key.charAt(2).toLowerCase() + key.slice(3);
-      Scant.prototype[chainFunc] = function () {
-        this.currentActions.push({
-          func: this[key],
-          args: arguments
-        });
-        return this;
-      };
-    }
+  Object.keys(Scant.prototype.do).forEach(function (key) {
+    Scant.prototype[key] = function () {
+      this.actions.push({
+        func: this.do[key],
+        args: arguments
+      });
+      return this;
+    };
   });
   
   $.fn.scant = function (defaults) {
--- tests/tests.js
+++ tests/tests.js
@@ -25,11 +25,11 @@ module("scant", {
 test("scant makes root position relative", function () {
   equal(this.$stage.css("position"), "relative", "root is position relative");
 });
-test("make creates doMake action", function () {
+test("make creates do.make action", function () {
   this.scant.make("#myid");
-  equal(this.scant.currentActions.length, 1, "has one action");
-  equal(this.scant.currentActions[0].func, this.scant.doMake, "action is doMake");
-  equal(this.scant.currentActions[0].args[0], "#myid", "arg is #myid");
+  equal(this.scant.actions.length, 1, "has one action");
+  equal(this.scant.actions[0].func, this.scant.do.make, "action is do.make");
+  equal(this.scant.actions[0].args[0], "#myid", "arg is #myid");
 });
 test("go executes one mock action", function () {
   var called = false, origArg = "myarg", gotArg;
@@ -49,15 +49,15 @@ test("go executes real action", function () {
   equal($("#myid", this.$stage).length, 1, "made div with id");    
 });
 test(".at() creates correct action", function () {
-  this.scant.currentActions = this.scant.actions = [{
+  this.scant.actions = [{
     func: function () {},
     args: ["myarg"]
   }];
   this.scant.at(100, 200);
-  equal(this.scant.currentActions.length, 2, "has two actions");
-  equal(this.scant.currentActions[1].func, this.scant.doAt, "action is doAt");
-  equal(this.scant.currentActions[1].args[0], 100, "first arg is correct");
-  equal(this.scant.currentActions[1].args[1], 200, "second arg is correct");
+  equal(this.scant.actions.length, 2, "has two actions");
+  equal(this.scant.actions[1].func, this.scant.do.at, "action is do.at");
+  equal(this.scant.actions[1].args[0], 100, "first arg is correct");
+  equal(this.scant.actions[1].args[1], 200, "second arg is correct");
 });
 test(".make().at().go() creates and positions node", function () {
   this.scant.make("#myid").at(100, 200).go();
@@ -66,79 +66,103 @@ test(".make().at().go() creates and positions node", function () {
   equal($(node).css("top"), "200px", "node top is correct");
 });
 
-test("doMake creates div with id, absolute position", function () {
-  this.scant.doMake("#myid");
+test("do.make creates div with id, absolute position", function () {
+  this.scant.do.make("#myid");
   var node = this.assertHasDiv("#myid");
   equal($(node).css("position"), "absolute", "node is absolutely positioned");
 });
-test("doMake creates class", function () {
-  this.scant.doMake(".myclass");
+test("do.make creates class", function () {
+  this.scant.do.make(".myclass");
   this.assertHasDiv(".myclass");
 });
-test("doMake creates node with ID before class", function () {
-  this.scant.doMake("#myid.myclass");
+test("do.make creates node with ID before class", function () {
+  this.scant.do.make("#myid.myclass");
   var node = this.assertHasDiv("#myid"),
       node2 = this.assertHasDiv(".myclass");
   equal(node, node2, "same div has both the id and class");
 });
-test("doMake creates node with class before ID", function () {
-  this.scant.doMake(".myclass#myid");
+test("do.make creates node with class before ID", function () {
+  this.scant.do.make(".myclass#myid");
   var node = this.assertHasDiv("#myid"),
       node2 = this.assertHasDiv(".myclass");
   equal(node, node2, "same div has both the id and class");
 });
-test("doMake creates node with ID and multiple classes", function () {
-  this.scant.doMake("#myid.class1.class2");
+test("do.make creates node with ID and multiple classes", function () {
+  this.scant.do.make("#myid.class1.class2");
   var node = this.assertHasDiv("#myid"),
       node2 = this.assertHasDiv(".class1"),
       node3 = this.assertHasDiv(".class2");
   equal(node, node2, "same div has both the id and class");
   equal(node2, node3, "same div has both classes");
 });
-test("doMake creates node with ID and text", function () {
-  this.scant.doMake("#myid some text");
+test("do.make creates node with ID and text", function () {
+  this.scant.do.make("#myid some text");
   var node = this.assertHasDiv("#myid");
   equal($(node).text(), "some text", "div has proper text");
 });
-test("doMake sets $current", function () {
-  this.scant.doMake("#myid");
+test("do.make sets $current", function () {
+  this.scant.do.make("#myid");
   var node = this.assertHasDiv("#myid");
-  equal(this.scant.$current[0], node, "created node is in $current");
+  equal(this.scant.do.$current[0], node, "created node is in $current");
 });
 
-test("doAt() positions current node", function () {
-  this.scant.$current = $("<div></div>");
-  this.scant.doAt(100, 200);
-  equal(this.scant.$current.css("left"), "100px", "left is correct");
-  equal(this.scant.$current.css("top"), "200px", "top is correct");
+test("do.at() positions current node", function () {
+  this.scant.do.$current = $("<div></div>");
+  this.scant.do.at(100, 200);
+  equal(this.scant.do.$current.css("left"), "100px", "left is correct");
+  equal(this.scant.do.$current.css("top"), "200px", "top is correct");
 });
-test("doSize() sets size of current node", function () {
-  this.scant.$current = $("<div></div>");
-  this.scant.doSize(200, 300);
-  equal(this.scant.$current.css("width"), "200px", "left is correct");
-  equal(this.scant.$current.css("height"), "300px", "top is correct");  
+test("do.size() sets size of current node", function () {
+  this.scant.do.$current = $("<div></div>");
+  this.scant.do.size(200, 300);
+  equal(this.scant.do.$current.css("width"), "200px", "left is correct");
+  equal(this.scant.do.$current.css("height"), "300px", "top is correct");  
 });
-test("doRightOf() places node default distance to right of target", function () {
-  this.scant.$current = $("<div></div>");
-  this.scant.doRightOf("#test-target");
-  equal(this.scant.$current.css("top"), "300px", "top aligned with target item");
-  equal(this.scant.$current.css("left"), "620px", "item is to right of target by default spacing");
+test("do.rightOf() places node default distance to right of target", function () {
+  this.scant.do.$current = $("<div></div>");
+  this.scant.do.rightOf("#test-target");
+  equal(this.scant.do.$current.css("top"), "300px", "top aligned with target item");
+  equal(this.scant.do.$current.css("left"), "620px", "item is to right of target by default spacing");
 });
-test("doLeftOf() places node default distance to left of target", function () {
-  this.scant.$current = $("<div></div>").css({ width: 100, height: 100 });
-  this.scant.doLeftOf("#test-target");
-  equal(this.scant.$current.css("top"), "300px", "top aligned with target item");
-  equal(this.scant.$current.css("left"), "80px", "item is to left of target by default spacing");
+test("do.leftOf() places node default distance to left of target", function () {
+  this.scant.do.$current = $("<div></div>").css({ width: 100, height: 100 });
+  this.scant.do.leftOf("#test-target");
+  equal(this.scant.do.$current.css("top"), "300px", "top aligned with target item");
+  equal(this.scant.do.$current.css("left"), "80px", "item is to left of target by default spacing");
 });
-test("doAbove() places node default distance above target", function () {
-  this.scant.$current = $("<div></div>").css({ width: 100, height: 100 });
-  this.scant.doAbove("#test-target");
-  equal(this.scant.$current.css("top"), "180px", "item is above target by default spacing");
-  equal(this.scant.$current.css("left"), "200px", "left aligned with target item");
+test("do.above() places node default distance above target", function () {
+  this.scant.do.$current = $("<div></div>").css({ width: 100, height: 100 });
+  this.scant.do.above("#test-target");
+  equal(this.scant.do.$current.css("top"), "180px", "item is above target by default spacing");
+  equal(this.scant.do.$current.css("left"), "200px", "left aligned with target item");
 });
-test("doBelow() places node default distance below target", function () {
-  this.scant.$current = $("<div></div>");
-  this.scant.doBelow("#test-target");
-  equal(this.scant.$current.css("top"), "820px", "item is below target by default spacing");
-  equal(this.scant.$current.css("left"), "200px", "left aligned with target item");
+test("do.below() places node default distance below target", function () {
+  this.scant.do.$current = $("<div></div>");
+  this.scant.do.below("#test-target");
+  equal(this.scant.do.$current.css("top"), "820px", "item is below target by default spacing");
+  equal(this.scant.do.$current.css("left"), "200px", "left aligned with target item");
+});
+
+asyncTest("do.next() returns promise resolved on next click", function () {
+  var finished = false;
+  this.scant.do.next().done(function () {
+    finished = true;
+    ok(true, "click resolved promise");
+    start();
+  });
+  
+  setTimeout(function () {
+    $(document.body).trigger("click");
+  }, 1);
+  setTimeout(function () {
+    if (!finished) {
+      ok(false, "promise never resolved");
+      start();
+    }
+  }, 20);
+});
+
+test("go() yields at next()", function () {
+  this.scant.next().make("#dont-make-me").go();
+  equal($("#dont-make-me", this.$stage).length, 0, "nothing executed after next()");
 });
